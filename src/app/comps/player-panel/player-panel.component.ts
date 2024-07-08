4c1fea2886fa6b70443361a4d755cab10524d53a
FelipeAlbarelli <felipesalbarelli@gmail.com>
2024-07-08 20:15:12 -0300

    basic layout

---
+++ src/app/comps/player-panel/player-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './player-panel.component.html',
+  styleUrl: './player-panel.component.css'
+})
+export class PlayerPanelComponent {
+
+}
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GameBoardComponent } from '../comps/game-board/game-board.component';
+import { PlayerPanelComponent } from '../comps/player-panel/player-panel.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [GameBoardComponent],
+  imports: [GameBoardComponent , PlayerPanelComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
